070e56aec61c363441a88d53388918421c17f8e2
Chenxi Mao <chenxi.mao2013@gmail.com>
2020-02-20 19:24:44 +0800

    fix build warnings

--- drivers/mmc/core/sdio_cis.c
+++ drivers/mmc/core/sdio_cis.c
@@ -29,6 +29,7 @@ static int cistpl_vers_1(struct mmc_card *card, struct sdio_func *func,
 {
 	unsigned i, nr_strings;
 	char **buffer, *string;
+	unsigned int length = 0;
 
 	/* Find all null-terminated (including zero length) strings in
 	   the TPLLV1_INFO field. Trailing garbage is ignored. */
@@ -55,7 +56,8 @@ static int cistpl_vers_1(struct mmc_card *card, struct sdio_func *func,
 
 	for (i = 0; i < nr_strings; i++) {
 		buffer[i] = string;
-		strlcpy(string, buf, strlen(buf) + 1);
+		length = strlen(buf) +1;
+		memcpy(string, buf, length);
 		string += strlen(string) + 1;
 		buf += strlen(buf) + 1;
 	}
